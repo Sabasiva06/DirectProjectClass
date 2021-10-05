6c5bb3633ae9a4f2aea5d6d5799c6fb13695c87c
Sabasiva06 <sabasiva06@gmail.com>
2021-10-06 00:40:14 +0530

    papom

--- src/org/day1/BrowserLaunch.java
+++ src/org/day1/BrowserLaunch.java
@@ -41,6 +41,8 @@ public class BrowserLaunch {
 		System.out.println("Asia");
 		System.out.println("India");
 		System.out.println("America");
+		System.out.println("summa");
+		System.out.println("summa");
 
 	}
 
